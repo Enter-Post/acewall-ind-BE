9a6119069179d78b798109688ac34fefdd62d251
Rubbas yar khan <rubbasyarkhan007@gmail.com>
2025-09-24 22:07:00 +0500

    landing page contact

---
+++ src/Routes/Contact.Routes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { sendSchoolcontactmail } from "../Contollers/contact.controller.js";
+
+const router = express.Router();
+
+router.post("/sendSchoolcontactmail", sendSchoolcontactmail)
+
+export default router;
+
