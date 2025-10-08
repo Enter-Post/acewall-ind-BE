53a24049accf41e0f39ad907e5a17d716cc58320
talhahasif <imtalha995@gmail.com>
2025-10-08 22:55:59 +0500

    requird remove

--- src/Models/PostModels/post.model.js
+++ src/Models/PostModels/post.model.js
@@ -3,7 +3,7 @@
 import mongoose from "mongoose"
 
 const Post = new mongoose.Schema({
-    text: { type: String, required: true },
+    text: { type: String },
     assets: [{ url: String, fileName: String }],
     author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     color: { type: String }
